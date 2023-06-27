1e18f96513a44747c022186a6e56a131b75fe846
dest-all <dest-all@outlook.com>
2023-06-27 22:20:27 +0700

    Corrected date time input.

--- src/DestallMaterials.Blazor.VisualTesting/Program.cs
+++ src/DestallMaterials.Blazor.VisualTesting/Program.cs
@@ -1,8 +1,6 @@
-using DestallMaterials.Blazor.Services.Extensions;
-using DestallMaterials.Blazor.Services.UI;
-using DestallMaterials.Blazor.VisualTesting;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using DestallMaterials.Blazor.VisualTesting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,8 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.ConfigureDestallBlazorServices();
-
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
